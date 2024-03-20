5d43d697a9061df9fe5a13aeb4aa2bf19c53de82
MostakemHossain <mostakememon123@gmail.com>
2024-03-20 20:49:42 +0600

    validate request middleware added

--- backend/src/app/modules/Admin/admin.routes.ts
+++ backend/src/app/modules/Admin/admin.routes.ts
@@ -1,11 +1,17 @@
 import express from "express";
+import validateRequest from "../../middlewares/validateRequest";
 import { adminControllers } from "./admin.controllers";
+import { adminValidationSchamas } from "./admin.validations";
 
 const router = express.Router();
 
 router.get("/", adminControllers.getAllAdminFromDB);
 router.get("/:id", adminControllers.getSingleAdminFromDb);
-router.patch("/:id", adminControllers.updateAdminIntoDb);
+router.patch(
+  "/:id",
+  validateRequest(adminValidationSchamas.updateAdmin),
+  adminControllers.updateAdminIntoDb
+);
 router.delete("/:id", adminControllers.deleteAdminFromDb);
 router.delete("/soft/:id", adminControllers.softDeleteAdminFromDb);
 
