6dc6fb143b2678c159f545651d117ce348cd554a
MostakemHossain <mostakememon123@gmail.com>
2024-03-22 22:46:56 +0600

    App Error Implement

---
+++ backend/src/app/middlewares/auth.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status";
+import config from "../../config";
+import { jwtHealpers } from "../../healpers/jwtHealpers";
+import AppError from "../errors/AppError";
+
+const auth = (...roles: string[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = req.headers.authorization;
+      if (!token) {
+        throw new AppError(httpStatus.UNAUTHORIZED, "You are not authorized");
+      }
+      const verifyUser = jwtHealpers.verifyToken(
+        token,
+        config.jwt.jwt_access_token_serect as string
+      );
+      if (!roles.length && roles.includes(verifyUser.role)) {
+        throw new AppError(httpStatus.FORBIDDEN, "Forbidden!");
+      }
+      next();
+    } catch (err) {
+      next(err);
+    }
+  };
+};
+export default auth;
--- backend/src/app/modules/User/user.routes.ts
+++ backend/src/app/modules/User/user.routes.ts
@@ -1,29 +1,7 @@
-import express, { NextFunction, Request, Response } from "express";
-import config from "../../../config";
-import { jwtHealpers } from "../../../healpers/jwtHealpers";
+import express from "express";
+import auth from "../../middlewares/auth";
 import { userControllers } from "./user.controllers";
 const router = express.Router();
-
-const auth = (...roles: string[]) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const token = req.headers.authorization;
-      if (!token) {
-        throw new Error("You are not authorized");
-      }
-      const verifyUser = jwtHealpers.verifyToken(
-        token,
-        config.jwt.jwt_access_token_serect as string
-      );
-      if (!roles.length && roles.includes(verifyUser.role)) {
-        throw new Error("You are not authorized");
-      }
-      next();
-    } catch (err) {
-      next(err);
-    }
-  };
-};
 
 router.post("/", auth("ADMIN", "SUPER_ADMIN"), userControllers.createAdmin);
 
