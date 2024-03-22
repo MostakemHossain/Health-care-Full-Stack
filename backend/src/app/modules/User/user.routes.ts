31f16e687abf3ee9a9fb345725977020ef147144
MostakemHossain <mostakememon123@gmail.com>
2024-03-22 21:22:35 +0600

    create admin with verify auth middleware

--- backend/src/app/modules/User/user.routes.ts
+++ backend/src/app/modules/User/user.routes.ts
@@ -1,7 +1,30 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import config from "../../../config";
+import { jwtHealpers } from "../../../healpers/jwtHealpers";
 import { userControllers } from "./user.controllers";
 const router = express.Router();
 
-router.post("/", userControllers.createAdmin);
+const auth = (...roles: string[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = req.headers.authorization;
+      if (!token) {
+        throw new Error("You are not authorized");
+      }
+      const verifyUser = jwtHealpers.verifyToken(
+        token,
+        config.jwt.jwt_access_token_serect as string
+      );
+      if (!roles.length && roles.includes(verifyUser.role)) {
+        throw new Error("You are not authorized");
+      }
+      next();
+    } catch (err) {
+      next(err);
+    }
+  };
+};
+
+router.post("/", auth("ADMIN", "SUPER_ADMIN"), userControllers.createAdmin);
 
 export const UserRoutes = router;
