51d52300acf0fd3718da6ef1bb9445759f349f88
MostakemHossain <mostakememon123@gmail.com>
2024-03-22 22:53:23 +0600

    give role based access route

--- backend/src/app/modules/Admin/admin.routes.ts
+++ backend/src/app/modules/Admin/admin.routes.ts
@@ -1,18 +1,25 @@
+import { UserRole } from "@prisma/client";
 import express from "express";
+import auth from "../../middlewares/auth";
 import validateRequest from "../../middlewares/validateRequest";
 import { adminControllers } from "./admin.controllers";
 import { adminValidationSchamas } from "./admin.validations";
 
 const router = express.Router();
 
-router.get("/", adminControllers.getAllAdminFromDB);
-router.get("/:id", adminControllers.getSingleAdminFromDb);
+router.get(
+  "/",
+  auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+  adminControllers.getAllAdminFromDB
+);
+router.get("/:id",auth(UserRole.ADMIN, UserRole.SUPER_ADMIN), adminControllers.getSingleAdminFromDb);
 router.patch(
   "/:id",
+  auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
   validateRequest(adminValidationSchamas.updateAdmin),
   adminControllers.updateAdminIntoDb
 );
-router.delete("/:id", adminControllers.deleteAdminFromDb);
-router.delete("/soft/:id", adminControllers.softDeleteAdminFromDb);
+router.delete("/:id",auth(UserRole.ADMIN, UserRole.SUPER_ADMIN), adminControllers.deleteAdminFromDb);
+router.delete("/soft/:id",auth(UserRole.ADMIN, UserRole.SUPER_ADMIN), adminControllers.softDeleteAdminFromDb);
 
 export const AdminRoutes = router;
--- backend/src/app/modules/User/user.routes.ts
+++ backend/src/app/modules/User/user.routes.ts
@@ -1,8 +1,13 @@
+import { UserRole } from "@prisma/client";
 import express from "express";
 import auth from "../../middlewares/auth";
 import { userControllers } from "./user.controllers";
 const router = express.Router();
 
-router.post("/", auth("ADMIN", "SUPER_ADMIN"), userControllers.createAdmin);
+router.post(
+  "/",
+  auth(UserRole.ADMIN, UserRole.ADMIN),
+  userControllers.createAdmin
+);
 
 export const UserRoutes = router;
