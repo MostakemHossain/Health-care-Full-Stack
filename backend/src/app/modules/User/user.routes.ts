935165b5c0db8eb7d3af405f74a44ca886557c07
MostakemHossain <mostakememon123@gmail.com>
2024-03-24 23:33:17 +0600

    file image upload in current working directory using multer

--- backend/src/app/modules/User/user.routes.ts
+++ backend/src/app/modules/User/user.routes.ts
@@ -1,5 +1,6 @@
 import { UserRole } from "@prisma/client";
 import express from "express";
+import { fileUploader } from "../../../healpers/fileUploader";
 import auth from "../../middlewares/auth";
 import { userControllers } from "./user.controllers";
 const router = express.Router();
@@ -7,6 +8,7 @@ const router = express.Router();
 router.post(
   "/",
   auth(UserRole.ADMIN, UserRole.ADMIN),
+  fileUploader.upload.single("file"),
   userControllers.createAdmin
 );
 
